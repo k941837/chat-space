e754aa1e1d49742ad0c078e21c8f73949ebb475e
k941837 <55436678+k941837@users.noreply.github.com>
2019-11-07 19:17:00 +0900

    AWS6

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,6 +9,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [600, 600]
   # process resize_to_fit: [300, 300]
 
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
